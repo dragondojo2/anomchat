c037c81cf6cbf58a7b38410e5039605da3eff734
dragondojo2 <dragondojo2@gmail.com>
2022-09-09 22:01:47 -0300

    user fix

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -5,6 +5,16 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
+    apiKey: process.env.VITE_FIREBASE_API_KEY,
+    authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.VITE_FIREBASE_APP_ID,
+    databaseURL: "https://react-anom-chat-default-rtdb.firebaseio.com/"
+};
+
+/* const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
     projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
@@ -12,7 +22,7 @@ const firebaseConfig = {
     messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
     appId: import.meta.env.VITE_FIREBASE_APP_ID,
     databaseURL: "https://react-anom-chat-default-rtdb.firebaseio.com/"
-};
+}; */
 
 const app = initializeApp(firebaseConfig)
 
